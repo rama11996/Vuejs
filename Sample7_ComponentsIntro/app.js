b2de16bec7d5ca84d6df54934892932ec4b4adcf
rama11996 <ramanathanpri@gmail.com>
2018-06-12 11:35:01 +0530

    variable declared

--- Sample7_ComponentsIntro/app.js
+++ Sample7_ComponentsIntro/app.js
@@ -1,9 +1,10 @@
+var data={
+  name: 'Rama'
+}
 Vue.component('greeting',{
   template:'<p> Hey its {{ name }} <button v-on:click="changeName">Click to change </button> </p>',
   data:function() {
-    return{
-      name:'Rama'
-    }
+    return data;
   },
   methods:{
     changeName:function(){
