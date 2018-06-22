5708d93111d1fae03cef77bc394deb248b737092
rama11996 <ramanathanpri@gmail.com>
2018-06-22 11:51:31 +0530

    Added Slot

---
+++ sample13_slots/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <formHelper>
+      <div slot="form-header">
+        <h3>Title of the form</h3>
+        <p>Info about the form</p>
+      </div>
+      <div slot="form-fileds">
+        <input type="text" name="name" placeholder="name" required>
+        <input type="password" name="password" placeholder="password" required>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click='handleSubmit'>Submit</button>
+      </div>
+    </formHelper>
+  </div>
+</template>
+
+<script>
+import formHelper from './components/formHelper.vue'
+
+
+export default {
+  components:{
+    'formHelper':formHelper
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
