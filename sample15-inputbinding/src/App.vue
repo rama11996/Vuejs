964fb8fbcf95c9a911e67c1a54a33bdedf0af633
rama11996 <ramanathanpri@gmail.com>
2018-06-25 13:08:34 +0530

    Checkboxes included

---
+++ sample15-inputbinding/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <addblog></addblog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addblog.vue'
+export default {
+  components:{
+    'addblog':addBlog
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ sample15-inputbinding/src/components/addblog.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id='add-blog'>
+    <h2>Add a new Blog post</h2>
+    <form>
+      <label>Blog Title</label>
+      <input type="text" v-model.lazy='blog.title' required>
+      <label>Blog Content</label>
+      <textarea v-model.lazy='blog.content'></textarea>
+      <div id="checkboxes">
+        <label>Playing</label>
+        <input type="checkbox" value="Playing" v-model="blog.categories">
+        <label>Roaming</label>
+        <input type="checkbox" value="Roaming" v-model="blog.categories">
+        <label>Fanning</label>
+        <input type="checkbox" value="Fanning" v-model="blog.categories">
+        <label>Reading</label>
+        <input type="checkbox" value="Reading" v-model="blog.categories">
+      </div>
+    </form>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title:  {{ blog.title }}</p>
+      <p>Blog Content: {{ blog.content }}</p>
+      <p>{{ blog.content }}</p>
+      <p> Blog Categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      blog:{
+        title:'',
+        content:'',
+        categories:[]
+    }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+h3{
+  margin-top: 10px;
+}
+#add-blog*{
+  box-sizing: border-box;
+}
+#add-blog{
+  margin: 20px auto;
+  max-width: 500px;
+}
+label{
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type='text'],textarea{
+  display:block;
+  margin: 8px;
+  width:100%;
+}
+#preview{
+  padding: 10px 20px;
+  border:1px solid #ddd;
+  margin: 30px 0;
+}
+#checkboxes label{
+  display:inline-block;
+
+}
+#checkboxes input{
+  display: inline-block;
+  margin-right:10px;
+}
+
+</style>
